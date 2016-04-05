30e63b6c6acd01cfce1fac032082c3ae2014f434
Gabe D <gpad88@gmail.com>
2016-04-05 15:48:53 -0700

    more carousel work

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -10,12 +10,30 @@ window.setupHome = function(){
   });
 
   $('.cover-item img').on('click', function(){
+    $('.project-detail').fadeOut();
     var _this = $(this)
     $('.cover-item img').css('border', 'none')
     _this.css("border", "10px solid black")
+    checkIfInView(_this)
     $('.arrow-down').remove()
     _this.after("<div class='arrow-down'></div>")
+    var detail = "#" + _this.data('project-name') + "-detail"
+    $(detail).fadeIn();
   });
+
+  function checkIfInView(element){
+    var elementLeftOffset = element.offset().left
+    var elementRightOffset = elementLeftOffset + element.width()
+
+    if(elementLeftOffset < 0){
+      $('#project-row').animate({scrollLeft: '-=460'}, 500);
+    }else if (elementRightOffset > $('#project-carousel').width()) {
+        $('#project-row').animate({scrollLeft: '+=460'}, 500);
+        return false;
+    }else{
+      return true;
+    }
+  }
 };
 
 $(document).ready(window.setupHome);
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,16 @@
 class StaticController < ApplicationController
   def home
+    @project_hash = {
+      smitten: {
+        thumbnail_url: 'https://s3-us-west-2.amazonaws.com/gabe-random/landing.jpg'
+      },
+      dlp: {
+        thumbnail_url: 'https://s3-us-west-2.amazonaws.com/gabe-random/dlphome.png'
+      },
+      hanoi: {
+        thumbnail_url: 'https://s3-us-west-2.amazonaws.com/gabe-random/hanoiss.png'
+      },
+    }
     render 'home'
   end
 end
